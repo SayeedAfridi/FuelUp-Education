e52691e4e8d37e721635a10fed43e460712d917d
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-09 18:17:39 +0600

    loading state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text, ActivityIndicator } from 'react-native'
 import colors from '../constants/colors'
 import L from '../constants/layout'
-const Button = ({ style, text, secondary, onPress, block }) => {
+const Button = ({ style, text, secondary, onPress, block, loading }) => {
   const styles = {
     paddingVertical: 14,
     paddingHorizontal: 24,
     backgroundColor: secondary ? colors.secondary : colors.primary,
     width: block ? '100%' : 'auto',
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: L.borderRadius,
     ...style,
   }
   return (
-    <TouchableOpacity style={styles} activeOpacity={0.8} onPress={onPress}>
+    <TouchableOpacity
+      disabled={loading}
+      style={styles}
+      activeOpacity={0.8}
+      onPress={onPress}>
+      {loading ? (
+        <ActivityIndicator
+          style={{ marginRight: 10 }}
+          size='small'
+          color='#fff'
+        />
+      ) : null}
       <Text
         style={{
           fontSize: 18,
